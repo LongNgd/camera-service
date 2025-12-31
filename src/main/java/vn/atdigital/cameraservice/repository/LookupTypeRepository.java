f175f9b4223b2a5c8804622d6b722650c9026d92
longngd <nguyendinhlong2112@gmail.com>
2025-12-31 14:43:06 +0700

    longnd-31122025-manyChanges

--- src/main/java/vn/atdigital/cameraservice/repository/LookupTypeRepository.java
+++ src/main/java/vn/atdigital/cameraservice/repository/LookupTypeRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.atdigital.cameraservice.domain.model.LookupType;
 
+import java.util.List;
+
 @Repository
 public interface LookupTypeRepository extends JpaRepository<LookupType, Long> {
 }
---
+++ src/main/java/vn/atdigital/cameraservice/service/impl/TypeValueServiceImpl.java
@@ -0,0 +1,38 @@
+package vn.atdigital.cameraservice.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import vn.atdigital.cameraservice.domain.DTO.TypeValueMapDTO;
+import vn.atdigital.cameraservice.domain.model.LookupType;
+import vn.atdigital.cameraservice.domain.model.LookupValue;
+import vn.atdigital.cameraservice.repository.LookupTypeRepository;
+import vn.atdigital.cameraservice.repository.LookupValueRepository;
+import vn.atdigital.cameraservice.service.TypeValueService;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TypeValueServiceImpl implements TypeValueService {
+    private final LookupTypeRepository lookupTypeRepository;
+    private final LookupValueRepository lookupValueRepository;
+
+    @Override
+    public List<TypeValueMapDTO> getTypeValueMap() {
+        List<LookupType> lookupTypeList = lookupTypeRepository.findAll();
+        List<LookupValue> lookupValueList = lookupValueRepository.findByIsActive(true);
+
+        Map<Long, List<LookupValue>> valueMapByTypeId  = lookupValueList.stream().collect(Collectors.groupingBy(LookupValue::getLookupTypeId));
+
+        return lookupTypeList.stream()
+                .map(lookupType -> TypeValueMapDTO.builder()
+                        .type(lookupType.getName())
+                        .valueList(valueMapByTypeId.getOrDefault(lookupType.getId(), Collections.emptyList())
+                                .stream()
+                                .map(LookupValue::getName)
+                                .collect(Collectors.toList()))
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
