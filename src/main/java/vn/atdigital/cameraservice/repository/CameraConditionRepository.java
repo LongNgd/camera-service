a839e946ea80b17a20178e401265ad49490426cd
longngd <nguyendinhlong2112@gmail.com>
2026-01-05 15:49:40 +0700

    longnd-05012026-addConditionRelatedApis

--- src/main/java/vn/atdigital/cameraservice/repository/CameraConditionRepository.java
+++ src/main/java/vn/atdigital/cameraservice/repository/CameraConditionRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.atdigital.cameraservice.domain.model.CameraCondition;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CameraConditionRepository extends JpaRepository<CameraCondition, Long> {
-    Optional<CameraCondition> findByCameraIdAndStatus(Long cameraId, Long active);
+    List<CameraCondition> findByCameraIdAndStatus(Long cameraId, Long active);
+
+    boolean existsByCameraIdAndProfileCodeAndStatus(Long cameraId, String profileCode, Long active);
+
+    Optional<CameraCondition> findByCameraIdAndProfileCodeAndStatus(Long cameraId, String profileCode, Long active);
 }
